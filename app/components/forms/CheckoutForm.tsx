a02e5e1d59efe490536ce144c54cce384188e908
Zach Goldberg <zachgoldberg@Zachs-MacBook-Pro.local>
2023-11-12 20:35:03 -0600

    fix url

--- app/components/forms/CheckoutForm.tsx
+++ app/components/forms/CheckoutForm.tsx
@@ -31,7 +31,7 @@ export default function CheckoutForm() {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: "http://localhost:3000",
+        return_url: "https://jewelrybygeorgia.com",
       },
     });
 
